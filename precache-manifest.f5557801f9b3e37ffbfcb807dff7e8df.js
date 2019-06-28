f3150fbb3c472a32db57f7c96b4e3b5491d08eee
Suraksha <suraksha.setty@gmail.com>
2019-06-28 14:05:24 -0400

    Updates

---
+++ precache-manifest.f5557801f9b3e37ffbfcb807dff7e8df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c659e92cdb7145ddb1b5ce5c19a72e",
+    "url": "/places/index.html"
+  },
+  {
+    "revision": "d66eb845366a596e1bbd",
+    "url": "/places/static/css/main.9f1dc473.chunk.css"
+  },
+  {
+    "revision": "2f5e3e098f5631685f0e",
+    "url": "/places/static/js/2.7016b429.chunk.js"
+  },
+  {
+    "revision": "d66eb845366a596e1bbd",
+    "url": "/places/static/js/main.ca7dd203.chunk.js"
+  },
+  {
+    "revision": "9be6bef8892f51246d90",
+    "url": "/places/static/js/runtime~main.374f439f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/places/static/media/logo.5d5d9eef.svg"
+  }
+]);
